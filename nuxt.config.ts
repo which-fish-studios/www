ae9e2a706306d48c7f075effbb877445f5b43b91
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-07-08 11:07:21 -0400

    Update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@ export default defineNuxtConfig({
   typescript: { strict: true },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  compatibilityDate: '2024-07-08',
+  vite: {
+    server: {
+      watch: {
+        ignored: ['**/.devenv/**', '**/.direnv/**'],
+      },
+    },
+  },
 })
