78608f21494553fd32271cbfee6a5db027602079
Jadi <jadilmiller@gmail.com>
2024-07-17 11:39:10 -0400

    Add unformatted content (names, title, links)

---
+++ components/MagicalManifest.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex h-screen flex-col">
+    <div
+      class="flex h-screen flex-col items-center justify-center space-y-4 bg-gray-950 p-8 text-white"
+    >
+      <h1 class="text-4xl md:text-6xl lg:text-8xl">Which Fish Studios</h1>
+      <a href="https://www.youtube.com/@whichfishstudios">YouTube</a>
+      <a href="https://twitter.com/whichfishgames">X</a>
+    </div>
+
+    <div
+      class="flex flex-col items-center justify-center space-y-4 bg-gray-950 p-8 text-white"
+    >
+      <h1 class="text-4xl md:text-5xl lg:text-8xl">Magical Manifest</h1>
+      <h2 class="text-2xl md:text-3xl lg:text-4xl">
+        Crazy Eights with a Deckbuilder Twist
+      </h2>
+      <p style="text-align: center; margin-left: 20px; margin-right: 20px">
+        Enjoy classic Crazy Eights game play with a deckbuilder twist! Match
+        cards by elemental suit or number. Compete with your wizard friends to
+        empty your hand the fastest. Win by emptying your hand as quickly as
+        possible, but watch as the losing players get rewarded by changing the
+        deck to their favor. Change the shared deck as you play! Enchant cards
+        to your advantage using runes from the shop. These runes turn normal
+        cards into special cards that can skip players, burn cards permanently,
+        and impact the game in numerous other magical ways. Buy new types of
+        special cards from the shop. Create synergies between your hand and the
+        deck by collecting relics, which only affect your hand. Make the deck
+        advantageous to you while being a nuisance to your friends.
+      </p>
+    </div>
+
+    <div
+      class="flex flex-col items-center justify-center space-y-4 bg-gray-950 p-8 text-white"
+    >
+      <h1 class="text-4xl md:text-5xl lg:text-8xl">Meet the Team</h1>
+      <h2 class="text-2xl md:text-3xl lg:text-4xl">Jeffrey Harmon</h2>
+      <p style="text-align: center; margin-left: 20px; margin-right: 20px">
+        Jeffrey Harmon is one of five founders of Sphere Province Games, who
+        published their debut game “Mission: Rovee” in the spring of 2024. He is
+        a recent computer science graduate that has worked on a few other
+        non-commercial projects such as HabiTile and STEM Career Game.
+      </p>
+      <h2 class="text-2xl md:text-3xl lg:text-4xl">Hunter Johns</h2>
+      <p style="text-align: center; margin-left: 20px; margin-right: 20px">
+        Hunter is a computer science student with a background in a little bit
+        of everything.
+      </p>
+      <h2 class="text-2xl md:text-3xl lg:text-4xl">Jadi Miller</h2>
+      <p style="text-align: center; margin-left: 20px; margin-right: 20px">
+        Jadi is a computer science student who has published several
+        non-commercial projects, including HabiTile and several smaller games on
+        Itch.
+      </p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <ComingSoon />
+  <MagicalManifest />
 </template>
