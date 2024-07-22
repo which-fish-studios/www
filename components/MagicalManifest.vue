f8be44e20deff76cb191167211c5677da82449b0
Jadi <jadilmiller@gmail.com>
2024-07-22 12:45:04 -0400

    Add team images and descriptions

--- components/MagicalManifest.vue
+++ components/MagicalManifest.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex h-screen flex-col">
+  <div class="flex h-screen flex-col bg-gray-950">
+    <!-- Title Section w/ Social Media Links -->
+
     <div
       class="flex h-screen flex-col items-center justify-center space-y-4 bg-gray-950 p-8 text-white"
     >
@@ -8,6 +10,8 @@
       <a href="https://twitter.com/whichfishgames">X</a>
     </div>
 
+    <!-- About Magical Manifest -->
+
     <div
       class="flex flex-col items-center justify-center space-y-4 bg-gray-950 p-8 text-white"
     >
@@ -15,6 +19,13 @@
       <h2 class="text-2xl md:text-3xl lg:text-4xl">
         Crazy Eights with a Deckbuilder Twist
       </h2>
+      <img
+        class="mx-auto h-24 w-24 rounded-full md:h-auto md:w-48 md:rounded-xl"
+        src="/assets/screenshot_cards.PNG"
+        alt=""
+        width="384"
+        height="512"
+      />
       <p style="text-align: center; margin-left: 20px; margin-right: 20px">
         Enjoy classic Crazy Eights game play with a deckbuilder twist! Match
         cards by elemental suit or number. Compete with your wizard friends to
@@ -30,28 +41,92 @@
       </p>
     </div>
 
-    <div
-      class="flex flex-col items-center justify-center space-y-4 bg-gray-950 p-8 text-white"
-    >
-      <h1 class="text-4xl md:text-5xl lg:text-8xl">Meet the Team</h1>
-      <h2 class="text-2xl md:text-3xl lg:text-4xl">Jeffrey Harmon</h2>
-      <p style="text-align: center; margin-left: 20px; margin-right: 20px">
-        Jeffrey Harmon is one of five founders of Sphere Province Games, who
-        published their debut game “Mission: Rovee” in the spring of 2024. He is
-        a recent computer science graduate that has worked on a few other
-        non-commercial projects such as HabiTile and STEM Career Game.
-      </p>
-      <h2 class="text-2xl md:text-3xl lg:text-4xl">Hunter Johns</h2>
-      <p style="text-align: center; margin-left: 20px; margin-right: 20px">
-        Hunter is a computer science student with a background in a little bit
-        of everything.
-      </p>
-      <h2 class="text-2xl md:text-3xl lg:text-4xl">Jadi Miller</h2>
-      <p style="text-align: center; margin-left: 20px; margin-right: 20px">
-        Jadi is a computer science student who has published several
-        non-commercial projects, including HabiTile and several smaller games on
-        Itch.
-      </p>
+    <!-- Meet the Team -->
+
+    <div>
+      <div class="text-center">
+        <h1 class="justify-center text-4xl text-white md:text-5xl lg:text-8xl">
+          Meet the Team
+        </h1>
+
+        <div>
+          <figure
+            class="rounded-xl bg-gray-950 p-8 md:flex md:p-0 dark:bg-slate-800"
+          >
+            <div class="space-y-4 pt-6 text-center md:p-8 md:text-left">
+              <blockquote>
+                <p class="text-lg font-medium text-white">
+                  Jeffrey Harmon is one of five founders of Sphere Province
+                  Games, who published their debut game “Mission: Rovee” in the
+                  spring of 2024. He is a recent computer science graduate that
+                  has worked on a few other non-commercial projects such as
+                  HabiTile and STEM Career Game.
+                </p>
+              </blockquote>
+              <figcaption class="font-medium">
+                <div class="text-sky-500 dark:text-sky-400">Jeffrey Harmon</div>
+                <div class="text-slate-700 dark:text-slate-500">Apogon</div>
+              </figcaption>
+            </div>
+          </figure>
+        </div>
+
+        <div>
+          <figure
+            class="rounded-xl bg-gray-950 p-8 md:flex md:p-0 dark:bg-slate-800"
+          >
+            <img
+              class="mx-auto h-24 w-24 rounded-full md:h-auto md:w-48 md:rounded-xl"
+              src="/assets/angler.png"
+              alt=""
+              width="384"
+              height="512"
+            />
+            <div class="space-y-4 pt-6 text-center md:p-8 md:text-left">
+              <blockquote>
+                <p class="text-lg font-medium text-white">
+                  Hunter is a computer science student with a background in a
+                  little bit of everything. He is primarily a developer for
+                  Which Fish Studios. He has worked on a few non-commercial
+                  games such as Snatch N Sniff.
+                </p>
+              </blockquote>
+              <figcaption class="font-medium">
+                <div class="text-sky-500 dark:text-sky-400">Hunter Johns</div>
+                <div class="text-slate-700 dark:text-slate-500">Anglerfish</div>
+              </figcaption>
+            </div>
+          </figure>
+        </div>
+
+        <div>
+          <figure
+            class="rounded-xl bg-gray-950 p-8 md:flex md:p-0 dark:bg-slate-800"
+          >
+            <img
+              class="mx-auto h-24 w-24 rounded-full md:h-auto md:w-48 md:rounded-xl"
+              src="/assets/wahoo.png"
+              alt=""
+              width="384"
+              height="512"
+            />
+            <div class="space-y-4 pt-6 text-center md:p-8 md:text-left">
+              <blockquote>
+                <p class="text-lg font-medium text-white">
+                  Jadi is a computer science student who has published several
+                  non-commercial projects, including HabiTile and several
+                  smaller games on Itch. She also puts together and films all of
+                  Which Fish Studios' Youtube videos.
+                </p>
+              </blockquote>
+              <figcaption class="font-medium">
+                <div class="text-sky-500 dark:text-sky-400">Jadi Miller</div>
+                <div class="text-slate-700 dark:text-slate-500">Wahoo</div>
+              </figcaption>
+            </div>
+          </figure>
+        </div>
+      </div>
     </div>
   </div>
 </template>
